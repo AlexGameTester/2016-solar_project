d9695450d8cfe6c0dfe2e9b34c913a0db0aa6bae
CunningNobleman <45738611+CunningNobleman@users.noreply.github.com>
2020-11-13 13:45:00 +0600

    Add files via upload
    experimental2

---
+++ exp2.py
@@ -0,0 +1,19 @@
+from solar_objects1 import Star, Planet
+star1 = Star()
+
+
+def write_space_objects_data_to_file(output_filename, space_objects):
+    """Сохраняет данные о космических объектах в файл.
+    Строки должны иметь следующий формат:
+    Star <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+    Planet <радиус в пикселах> <цвет> <масса> <x> <y> <Vx> <Vy>
+
+    Параметры:
+
+    **output_filename** — имя входного файла
+    **space_objects** — список объектов планет и звёзд
+    """
+    with open(output_filename, 'w') as out_file:
+        for obj in space_objects:
+            print(out_file, "%s %d %s %f %f %f %f %f" % ('Star', obj.R, obj.color, obj.m, obj.x, obj.y, obj.Vx, obj.Vy))
+write_space_objects_data_to_file("exp2.txt", [star1] )            
