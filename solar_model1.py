93a8b3bce71176872f62748c16c1183d1cc2b512
CunningNobleman <45738611+CunningNobleman@users.noreply.github.com>
2020-11-13 13:04:22 +0600

    Upd SolarObjects1
    added formula for Fx, Fy calculation.

--- solar_model1.py
+++ solar_model1.py
@@ -19,8 +19,10 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += 1  
-        body.Fy += 2  
+        cosA = (- body.x + obj.x)/r
+        sinA = (- body.y + obj.y)/r
+        body.Fx += (body.m * obj.m)/(r**2) * cosA
+        body.Fy += (body.m * obj.m)/(r**2) * sinA 
 
 
 def move_space_object(body, dt):
