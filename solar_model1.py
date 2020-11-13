60fcf1c1dfda6820e7f422ae58905bc7c9e4c125
CunningNobleman <45738611+CunningNobleman@users.noreply.github.com>
2020-11-13 12:51:44 +0600

    Update solar_model1.py

--- solar_model1.py
+++ solar_model1.py
@@ -33,9 +33,9 @@ def move_space_object(body, dt):
 
     ax = body.Fx/body.m
     ay = body.Fy/body.m
-    body.x += Vx*dt
+    body.x += body.Vx*dt
     body.Vx += ax*dt
-    body.y += Vy*dt
+    body.y += body.Vy*dt
     body.Vy += ay*dt
     
 
