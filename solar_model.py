edb6c62df14659a521ffcc22af5551d4ef5d14eb
AlexGameTester <l.kirka@mail.ru>
2020-11-17 19:45:28 +0300

    Runge-Kutta method sketch

--- solar_model.py
+++ solar_model.py
@@ -5,7 +5,7 @@ gravitational_constant = 6.67408E-11
 """Гравитационная постоянная Ньютона G"""
 
 
-def calculate_force(body, space_objects):
+def calculate_force(body, space_objects, local_x, local_y):
     """Вычисляет силу, действующую на тело.
 
     Параметры:
@@ -15,12 +15,74 @@ def calculate_force(body, space_objects):
     """
 
     body.Fx = body.Fy = 0
+    fx = 0
+    fy = 0
     for obj in space_objects:
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
-        r = ((body.x - obj.x)**2 + (body.y - obj.y)**2)**0.5
-        body.Fx += gravitational_constant * body.m * obj.m / r**3 * (obj.x - body.x)
-        body.Fy +=  gravitational_constant * body.m * obj.m / r**3 * (obj.y - body.y)
+        r_x = ((local_x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+        r_y = ((body.x - obj.x)**2 + (local_y - obj.y)**2)**0.5
+        fx += gravitational_constant * body.m * obj.m / r_x**3 * (obj.x - local_x)
+        fy += gravitational_constant * body.m * obj.m / r_y**3 * (obj.y - local_y)
+
+    return (fx, fy)
+
+
+def _calc_ax(body, space_objects, local_x):
+    ax = 0
+    for obj in space_objects:
+        if body == obj:
+            continue  # тело не действует гравитационной силой на само себя!
+        r_x = ((local_x - obj.x)**2 + (body.y - obj.y)**2)**0.5
+        ax += gravitational_constant * obj.m / r_x**3 * (obj.x - local_x)
+
+    return ax
+
+
+def _calc_ay(body, space_objects, local_y):
+    ay = 0
+    for obj in space_objects:
+        if body == obj:
+            continue  # тело не действует гравитационной силой на само себя!
+        r_y = ((body.x - obj.x)**2 + (local_y - obj.y)**2)**0.5
+        ay += gravitational_constant * obj.m / r_y**3 * (obj.y - local_y)
+
+    return ay
+
+
+def move_space_object_rk(body, space_objects, dt):
+    ax = lambda x: _calc_ax(body, space_objects, x)
+    k1x = dt * ax(body.x)
+    q1x = dt * body.Vx
+
+    k2x = dt * ax(body.x + q1x / 2)
+    q2x = dt * (body.Vx + k1x / 2)
+
+    k3x = dt * ax(body.x + q2x / 2)
+    q3x = dt * (body.Vx + k2x / 2)
+
+    k4x = dt * ax(body.x + q3x)
+    q4x = dt * (body.Vx + k3x)
+
+    body.Vx += (k1x + 2 * k2x + 2 * k3x + k4x) / 6
+    body.x += (q1x + 2 * q2x + 2 * q3x + q4x) / 6
+
+    ay = lambda y: _calc_ay(body, space_objects, y)
+    k1y = dt * ay(body.y)
+    q1y = dt * body.Vy
+
+    k2y = dt * ay(body.y + q1y / 2)
+    q2y = dt * (body.Vy + k1y / 2)
+
+    k3y = dt * ay(body.y + q2y / 2)
+    q3y = dt * (body.Vy + k2y / 2)
+
+    k4y = dt * ay(body.y + q3y)
+    q4y = dt * (body.Vy + k3y)
+
+    body.Vy += (k1y + 2 * k2y + 2 * k3y + k4y) / 6
+    body.y += (q1y + 2 * q2y + 2 * q3y + q4y) / 6
+
 
 
 def move_space_object(body, dt):
@@ -50,9 +112,12 @@ def recalculate_space_objects_positions(space_objects, dt):
     """
 
     for body in space_objects:
-        calculate_force(body, space_objects)
-    for body in space_objects:
-        move_space_object(body, dt)
+        move_space_object_rk(body, space_objects, dt)
+
+    # for body in space_objects:
+    #     calculate_force(body, space_objects)
+    # for body in space_objects:
+    #     move_space_object(body, dt)
 
 
 if __name__ == "__main__":
